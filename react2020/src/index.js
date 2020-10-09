391d67995350dab330c80dd0812e009f897f2760
Danielle Byrne <DanielleByrne421@gmail.com>
2020-10-09 19:06:02 -0400

    fixed invalid property name

--- react2020/src/index.js
+++ react2020/src/index.js
@@ -2,17 +2,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-function getButtonText(){
-    return 'click here!'
+function getButtonText() {
+  return "click here!";
 }
 // create a react component
 const App = function () {
   return (
     <div>
-      <label for="name"> Enter Name:</label>
+      <label className="label" htmlFor="name">
+        {" "}
+        Enter Name:
+      </label>
       <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+      <button style={{ backgroundColor: "blue", color: "white" }}>
         {getButtonText()}
       </button>
     </div>
