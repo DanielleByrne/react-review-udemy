b7f3d53c0ac701649c77c71e2de472e656895497
Danielle Byrne <DanielleByrne421@gmail.com>
2020-10-09 18:53:50 -0400

    adding a function call in the jsx for the button text

--- react2020/src/index.js
+++ react2020/src/index.js
@@ -2,13 +2,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+function getButtonText(){
+    return 'click here!'
+}
 // create a react component
 const App = function () {
   return (
     <div>
       <label for="name"> Enter Name:</label>
       <input id="name" type="text" />
-      <button style={{backgroundColor:'blue', color:'white'}}> Submit</button>
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {getButtonText()}
+      </button>
     </div>
   );
 };
