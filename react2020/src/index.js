2e1779f8c49fdebf34b44fd48e8ba952754a5557
Danielle Byrne <DanielleByrne421@gmail.com>
2020-10-10 21:45:17 -0400

    adding a time and avatar image as props, finishing video 36

--- react2020/src/index.js
+++ react2020/src/index.js
@@ -1,14 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author= "Sam"/>
-      <CommentDetail author="Dan"/>
-      <CommentDetail author="Ian"/>
-      <CommentDetail author="Jan"/>
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:35pm"
+        content="I had tacos yeserday"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Dan"
+        timeAgo="Today at 7:36pm"
+        content="omg I love tacos!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Ian"
+        timeAgo="Yesterday at 10:40am"
+        content="I'm more of a pizza guy tbh"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jan"
+        timeAgo="Yesterday at 5:48pm"
+        content="All I want in life is endless tacos"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
