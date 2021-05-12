8cd57142a22f4c1798b28a0b791f8e3ec25bcde3
Danielle Byrne <DanielleByrne421@gmail.com>
2021-05-11 23:42:23 -0700

    adding more comments to have better idea of why need configurable components

--- react2020/src/index.js
+++ react2020/src/index.js
@@ -19,6 +19,34 @@ const App = () => {
           <div className="text"> A blog post </div>
         </div>
       </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Steve
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 8:00pm</span>
+          </div>
+          <div className="text"> A blog post </div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sally
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 8:00pm</span>
+          </div>
+          <div className="text"> A blog post </div>
+        </div>
+      </div>
     </div>
   );
 };
