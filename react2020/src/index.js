e76b7ff9413a99288b3c36d8797eff7f0a0c8a4a
Danielle Byrne <DanielleByrne421@gmail.com>
2021-05-11 23:07:46 -0700

    clearing out old files and starting new one for new section

--- react2020/src/index.js
+++ react2020/src/index.js
@@ -1,48 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
-import faker from "faker";
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail
-          author="Sam"
-          timeAgo="Today at 4:35pm"
-          content="I had tacos yeserday"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Dan"
-          timeAgo="Today at 7:36pm"
-          content="omg I love tacos!"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail
-          author="Ian"
-          timeAgo="Yesterday at 10:40am"
-          content="I'm more of a pizza guy tbh"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail
-          author="Jan"
-          timeAgo="Yesterday at 5:48pm"
-          content="All I want in life is endless tacos"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-    </div>
-  );
+  return <div>pineapples</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
