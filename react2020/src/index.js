34cf0e92da28f1bcea90993e3fde4b9e27efbcaf
Danielle Byrne <DanielleByrne421@gmail.com>
2021-05-13 08:42:55 -0700

    created a comment detail component and moved a comment into it

--- react2020/src/index.js
+++ react2020/src/index.js
@@ -11,20 +11,6 @@ const App = () => {
         </a>
         <div className="content">
           <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 8:00pm</span>
-          </div>
-          <div className="text"> A blog post </div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
             Steve
           </a>
           <div className="metadata">
