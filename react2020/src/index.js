cd5dde3f62f90d7e6e4fb29cd688503dcacf2c62
Danielle Byrne <DanielleByrne421@gmail.com>
2021-05-13 20:54:32 -0700

    added props to the components

--- react2020/src/index.js
+++ react2020/src/index.js
@@ -5,7 +5,9 @@ import CommentDetail from "./CommentDetail"
 const App = () => {
   return (
     <div className="ui container comments">
-        <CommentDetail /> 
+        <CommentDetail author="Sam"/> 
+        <CommentDetail author="Sally"/> 
+        <CommentDetail author="Nat"/> 
     </div>
   );
 };
