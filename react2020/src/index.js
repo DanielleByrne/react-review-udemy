9c44298a2b3fc6668d39d06ee6cf6eb41f514783
Danielle Byrne <DanielleByrne421@gmail.com>
2021-05-20 00:24:33 -0700

    starting initial files for new section

--- react2020/src/index.js
+++ react2020/src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
-import faker from "faker";
-import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-  return;
+  return <div>hi</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
