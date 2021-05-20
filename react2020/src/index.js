c77d656e7635adc881a004a75ccfc21ccca27676
Danielle Byrne <DanielleByrne421@gmail.com>
2021-05-20 00:18:45 -0700

    removing files for the new section of udemy course

--- react2020/src/index.js
+++ react2020/src/index.js
@@ -5,30 +5,7 @@ import faker from "faker";
 import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail
-          author="Sam"
-          timeAgo="today at 5:00"
-          comment="words"
-          profileImage={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <CommentDetail
-        author="Sally"
-        timeAgo="today at 7:00"
-        comment="bulgogi"
-        profileImage={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Nat"
-        timeAgo="today at 9:00"
-        comment="apples"
-        profileImage={faker.image.avatar()}
-      />
-    </div>
-  );
+  return;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
